05827b847d795747556e1d2a205db55b9dba0863
hanalaydrus <hanalaydrus@yahoo.com>
2015-09-17 05:49:35 +0700

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,7 +1,7 @@
 # 18213022-18213028
 Integrative Programming 
 
-package tcp;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,36 +23,17 @@ public class Client {
      * it serves.
      */
     public static void main(String[] args) throws IOException {
-//        String serverAddress = JOptionPane.showInputDialog(
-//            "Enter IP Address of a machine that is\n" +
-//            "running the date service on port 9090:");
-        Socket s = new Socket("192.168.1.122", 8888);
- //       BufferedReader input =
- //           new BufferedReader(new InputStreamReader(s.getInputStream()));
- //       String answer = input.readLine();
- //       JOptionPane.showMessageDialog(null, answer);
-/*        String message = JOptionPane.showInputDialog("Please enter your message :");
+//  
+        Socket s = new Socket("192.168.1.122", 1234);
+ //      
+        String message = JOptionPane.showInputDialog("Please enter your message :");
         try {
             PrintWriter out =
                 new PrintWriter(s.getOutputStream(), true);
             out.println(message);
         } finally {
             s.close();
-        }*/
-        
-        System.out.println("Just connected to " + s.getRemoteSocketAddress()); 
-		PrintWriter toServer = 
-			new PrintWriter(s.getOutputStream(),true);
-		BufferedReader fromServer = 
-			new BufferedReader(
-					new InputStreamReader(s.getInputStream()));
-		toServer.println("Hello from " + s.getLocalSocketAddress()); 
-		String line = fromServer.readLine();
-		System.out.println("Client received: " + line + " from Server");
-		toServer.close();
-		fromServer.close();
-		s.close();
-        
+        }
         System.exit(0);
     }
 }
