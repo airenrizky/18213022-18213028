674229a77dd4cef68ae7fbff0b8ea934969ef35b
Airen Rizky Fauziandini <airenrizky@s.itb.ac.id>
2015-11-03 18:45:22 +0700

    Create SOAP_Server.php

---
+++ SOAP_Server.php
@@ -0,0 +1,11 @@
+<?php
+
+	$opt = array('location' => 'http://localhost/SOAP-Server.php', 'uri' => 'http://localhost/');
+	
+	$api = new SoapClient(NULL, $opt);
+	echo $api -> helloworld();
+	echo $api -> addition(12,10);
+	echo ("<br>");
+	echo ("<h1>Data yang diambil dari tabel siswa pada database perkuliahan</h1>");
+	echo $api -> getData();
+?>
